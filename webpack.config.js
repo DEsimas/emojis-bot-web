303b9d2671dc577f07bdaa28a358409ace767305
DEsimas <simasuh@gmail.com>
2023-05-01 11:56:22 +0300

    rename index.js to index.jsx

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/index.jsx',
     target: 'web',
     mode: process.env.MODE,
     module: {
