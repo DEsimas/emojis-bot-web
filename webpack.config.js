4b46c5322772a20f7aa6a78df0473524ee4a98eb
DEsimas <simasuh@gmail.com>
2023-05-01 12:05:13 +0300

    create .editorconfig

--- webpack.config.js
+++ webpack.config.js
@@ -3,55 +3,55 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.jsx',
-    target: 'web',
-    mode: process.env.MODE,
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ["@babel/preset-react"],
-                    },
-                },
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            },
-            {
-                test: /\.(gif|png|jpe?g|svg)$/i,
-                use: [
-                    'file-loader',
-                ],
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            favicon: './public/favicon.ico'
-        })
-    ],
-    output: {
-        publicPath: '/build/',
-        path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js'
-    },
-    devServer: {
-        static: {
-            directory: path.resolve(__dirname, 'build'),
+  entry: './src/index.jsx',
+  target: 'web',
+  mode: process.env.MODE,
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react"],
+          },
         },
-        port: process.env.PORT,
-        historyApiFallback: true,
-        open: true,
-        hot: true
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+        ],
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      favicon: './public/favicon.ico'
+    })
+  ],
+  output: {
+    publicPath: '/build/',
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'build'),
     },
+    port: process.env.PORT,
+    historyApiFallback: true,
+    open: true,
+    hot: true
+  },
 }
